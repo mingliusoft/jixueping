6e0e14919dccb16a2357fff038c67f681d78570e
mingliusoft <45680313+mingliusoft@users.noreply.github.com>
2018-12-08 02:15:25 +0000

    demo2

--- pscloud_training/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ pscloud_training/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'PS Cloud 培训',
+    'name': 'PS Cloud 培训23',
     'version': '12.0.1.0',
     'summary': 'PS Cloud 培训',
     'author': "www.mypscloud.com",
@@ -9,14 +9,9 @@
     'depends': ['base'],
     'data': [
         'security/ir.model.access.csv',
-        'wizard/student_regiester_views.xml',
-        'views/res_partner_views.xml',
-        'views/training_lesson_views.xml',
-        'views/training_subject_views.xml',
         'views/training_views.xml',
     ],
     'demo': [
-        'demo/pscloud_demo.xml',
     ],
     'qweb': [],
     'js': [],
