d5e504a823a7bba135e498991bfe5f1e7dd6286f
mingliusoft <45680313+mingliusoft@users.noreply.github.com>
2018-12-08 02:11:18 +0000

    demo1

--- pscloud_training/models/subject.py
+++ pscloud_training/models/subject.py
@@ -3,12 +3,12 @@
 from odoo import api, fields, models
 
 class TrainingSubject(models.Model):
-    _name = 'pscloud.training.subject'
+    _name = 'mingliusoft_dev_training.training.subject'
     _description = "科目"
 
     name = fields.Char(string='名称')
     person_id = fields.Many2one('res.partner', string='负责人')
-    lesson_ids = fields.One2many('pscloud.training.lesson', 'subject_id', string='课程')
+    lesson_ids = fields.One2many('mingliusoft_dev_training.training.lesson', 'subject_id', string='课程')
     desc = fields.Text(string='描述')
 
 #  vim:et:si:sta:ts=4:sts=4:sw=4:tw=79:
